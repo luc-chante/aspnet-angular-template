bade0e63669855bb31d890fc2c315e6cbe32c6c1
Luc Chante <luc.chante@gmail.com>
2016-05-19 01:16:11 +0200

    Project files

---
+++ TemplateAngular/Scripts/main.component.ts
@@ -0,0 +1,7 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'main',
+    template: '<h1>This is a template for Angular 2.0 with MVC 6</h1>'
+})
+export class MainComponent { }
---
+++ TemplateAngular/Scripts/main.ts
@@ -0,0 +1,4 @@
+﻿import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { MainComponent } from './main.component';
+
+bootstrap(MainComponent);
